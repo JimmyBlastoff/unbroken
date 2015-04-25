d23c781d79491786b3ca2c911d5ec3ac42247bb5
JimmyBlastoff <JimmyBlastoff666@gmail.com>
2015-04-24 21:12:41 -0500

    Missed Icons

--- share/qt/make_windows_icon.sh
+++ share/qt/make_windows_icon.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # create multiresolution windows icon
-ICON_SRC=../../src/qt/res/icons/bitcoin.png
-ICON_DST=../../src/qt/res/icons/bitcoin.ico
-convert ${ICON_SRC} -resize 16x16 bitcoin-16.png
-convert ${ICON_SRC} -resize 32x32 bitcoin-32.png
-convert ${ICON_SRC} -resize 48x48 bitcoin-48.png
-convert bitcoin-16.png bitcoin-32.png bitcoin-48.png ${ICON_DST}
+ICON_SRC=../../src/qt/res/icons/unbrokencoin.png
+ICON_DST=../../src/qt/res/icons/unbrokencoin.ico
+convert ${ICON_SRC} -resize 16x16 unbrokencoin-16.png
+convert ${ICON_SRC} -resize 32x32 unbrokencoin-32.png
+convert ${ICON_SRC} -resize 48x48 unbrokencoin-48.png
+convert unbrokencoin-16.png unbrokencoin-32.png unbrokencoin-48.png ${ICON_DST}
 
