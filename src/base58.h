30aa8c7d38957200df64005f215b69f9cbd7bcfb
JimmyBlastoff <JimmyBlastoff666@gmail.com>
2015-04-24 19:11:33 -0500

    Pub Key address update

--- src/base58.h
+++ src/base58.h
@@ -272,8 +272,8 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 0,
-        SCRIPT_ADDRESS = 5,
+        PUBKEY_ADDRESS = 68,
+        SCRIPT_ADDRESS = 30,
         PUBKEY_ADDRESS_TEST = 111,
         SCRIPT_ADDRESS_TEST = 196,
     };
